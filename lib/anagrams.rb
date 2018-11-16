ee9b2e26f322f83a05eef0da243ccf03cb13b3de
Jared Reando <jaredreando@gmail.com>
2018-11-16 10:08:24 -0800

    Add check for non-alphanumeric characters in input strings. If detected, return error message.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,6 +3,12 @@ require('pry')
 # class Word < String
 
   def anagrams(word1, word2)
+    if(/\W|\d/.match(word1))
+      return "'#{word1}' is not a word."
+    elsif(/\W|\d/.match(word2))
+      return "'#{word2}' is not a word."
+    end
+
     if(word1.downcase.split("").sort().join() == word2.downcase.split("").sort().join())
     "This is a match"
     else
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -6,8 +6,27 @@ describe('anagrams method') do
     expect(anagrams("i", "i")).to(eq("This is a match"))
   end
 
-  it("evaluates two words for a match (not case sensitive).") do
-    expect(anagrams("Apple bottom jeans", "apple jeans bottom")).to(eq("This is a match"))
+  it("evaluates two words for an exact match regardless of case.") do
+    expect(anagrams("apple", "aPplE")).to(eq("This is a match"))
   end
+
+  it("evaluates two words for an anagram match.") do
+    expect(anagrams("sham", "Mash")).to(eq("This is a match"))
+  end
+
+  it("checks if inputs are words before comparing values.") do
+    expect(anagrams("shameless", "sham3less")).to(eq("'sham3less' is not a word."))
+  end
+
+
+  # check if two words are anagrams
+  #
+  # account for possibility that they could be different cases
+  #
+  # check if inputs are words. A word must contain at least 1 vowel.
+  #
+  # check if they are antigrams. That is, do the two words share NO matching letters?
+  #
+  # account for multiple words, spaces, and punctuation. evaluate entire collection of letters as one input 'pool'
 
 end
