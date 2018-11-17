381f192f7ea2203c6ddd5e0731d10bfc1dc1ac98
Jared Reando <jaredreando@gmail.com>
2018-11-16 16:24:29 -0800

    Update README.md. Refactor code and fix bugs for edge cases.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -7,31 +7,44 @@ class Word < String
   end
 
 
-  def non_letters?(compare_word)
-    (/\W|\d/.match(@string)) | !(/[aeiouy]/.match(@string))
+  def non_letters?(letters_compare_word)
+    (/\W|\d/.match(letters_compare_word)) || !(/[aeiouy]/.match(letters_compare_word))
   end
 
-  def antigram?(compare_word)
-    (/["#{@string}"]/.match("#{compare_word}"))
+  def real_word?(real_word_compare_word)
+    (/\W|\d/.match(real_word_compare_word))
   end
 
-  def real_word?(compare_word)
-    (/\W|\d/.match(compare_word))
+  def antigram?(antigram_compare_word)
+    (/["#{@string}"]/.match("#{antigram_compare_word}"))
   end
+
 
   def anagrams(compare_word)
-    if(non_letters?(compare_word))
-      return "'#{@string}' is not a word."
-    elsif (real_word?(compare_word))
+    compares = [@string, compare_word]
+
+    if(non_letters?(compare_word.gsub(" ","")))
       return "'#{compare_word}' is not a word."
+    elsif(non_letters?(@string.gsub(" ","")))
+      return "'#{@string}' is not a word."
     end
+
+# checks if either word contains non-alpha characters. If that check is true, statement shows
+    # if (real_word?(compare_word.gsub(" ","")))
+    #   return "'#{compare_word}' is not a word."
+    # elsif (real_word?(@string.gsub(" ","")))
+    #   return "'#{@string}' is not a word."
+    # end
+
     if !(antigram?(compare_word))
       return "'#{@string}' and '#{compare_word}' are antigrams."
     end
+
     if(@string.downcase.split("").sort().join() == compare_word.downcase.split("").sort().join())
     "'#{@string}' and '#{compare_word}' are anagrams!"
     else
     "No match"
     end
+
   end
 end
